7c72be116e0ac0d134e2addc6f86960c193913d5
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2012-11-19 20:30:30 -0800

    Set shell file name.

--- x-hugh-custom.el
+++ x-hugh-custom.el
@@ -29,9 +29,16 @@
  '(require-final-newline t)
  '(scroll-bar-mode -1)
  '(setq c-basic-offset)
+ '(shell-file-name "/bin/bash")
  '(show-paren-mode 1)
  '(spell-command "ispell")
  '(split-window-preferred-function nil)
  '(standard-indent 8)
  '(w3m-use-cookies t)
  '(wg-morph-on nil))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
