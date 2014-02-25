18f31222927095158e0799a058c4d1ffe25e2289
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-02-24 20:38:05 -0800

    Merge stuff in from work.

--- x-hugh-finally.el
+++ x-hugh-finally.el
@@ -3,9 +3,7 @@
 (quietly-read-abbrev-file)
 (server-start)
 (setq default-major-mode 'text-mode)
-
-(wg-load "~/.emacs.d/workgroups-save.el")
-
+(wg-load "~/.emacs.d/workgroups_save.el")
 ;; Lastly, like fortune:
 (totd)
 
