396ac48c10bad6dff682f22b618693374d0b99bf
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-04-26 09:05:08 -0700

    Add x-hugh-packages.el.

---
+++ x-hugh-packages.el
@@ -0,0 +1,8 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+(provide 'x-hugh-packages)
