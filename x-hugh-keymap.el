2e147a9ca8e37f6ea1ff5ccc910b2959a2e70464
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-08-14 06:28:48 -0700

    Toggle.

--- x-hugh-keymap.el
+++ x-hugh-keymap.el
@@ -1,5 +1,24 @@
 ;; Keymaps
 
+;; From http://endlessparentheses.com/the-toggle-map-and-wizardry.html
+;; Other suggestons: http://irreal.org/blog/?p=2830
+
+(define-prefix-command 'endless/toggle-map)
+;; The manual recommends C-c for user keys, but C-x t is
+;; always free, whereas C-c t is used by some modes.
+(define-key ctl-x-map "t" 'endless/toggle-map)
+(define-key endless/toggle-map "c" 'column-number-mode)
+(define-key endless/toggle-map "d" 'toggle-debug-on-error)
+(define-key endless/toggle-map "e" 'toggle-debug-on-error)
+(define-key endless/toggle-map "f" 'auto-fill-mode)
+(define-key endless/toggle-map "l" 'toggle-truncate-lines)
+(define-key endless/toggle-map "q" 'toggle-debug-on-quit)
+(define-key endless/toggle-map "t" 'endless/toggle-theme)
+;;; Generalized version of `read-only-mode'.
+(define-key endless/toggle-map "r" 'dired-toggle-read-only)
+(autoload 'dired-toggle-read-only "dired" nil t)
+
+
 ;; open confluence page
 (global-set-key "\C-xwf"	'confluence-get-page)
 (global-set-key "\C-xwc"	'confluence-create-page)
