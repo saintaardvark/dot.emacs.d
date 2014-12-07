622d0bcbdb6f433967fce1d44183bfe432970785
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-12-06 16:20:43 -0800

    Helm-m-x too slow on tethys.

--- x-hugh-modes.el
+++ x-hugh-modes.el
@@ -40,7 +40,7 @@
   (progn
     (require 'helm-config nil 'noerror)
     (helm-mode 1)
-    (global-set-key (kbd "M-x")     'helm-M-x)
+    ;; (global-set-key (kbd "M-x")     'helm-M-x)
     (global-set-key (kbd "M-y")     'helm-show-kill-ring)
     (global-set-key (kbd "C-x b")   'helm-mini)
     (global-set-key (kbd "C-x C-f") 'helm-find-files)))
