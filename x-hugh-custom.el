0d96b61588c6285966751d6a4af7ba31d20265e4
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-12-11 05:12:07 -0800

    Big cleanup; this was corrupted.

--- x-hugh-custom.el
+++ x-hugh-custom.el
@@ -40,9 +40,54 @@
  '(org-agenda-start-with-clockreport-mode t)
  '(org-agenda-start-with-follow-mode t)
  '(org-agenda-start-with-log-mode t)
- '(org-capture-templates (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-rmail org-vm org-wl org-w3m) (("R" "Autofill RT ticket" entry (file "~/org/all.org") "** TODO [#A] %?") ("r" "RT Ticket" entry (file "~/org/all.org") "** TODO RT #%^{Number} -- %^{Subject}") ("t" "TODO" entry (file "~/org/all.org") "** TODO %^{TODO}") ("q" "Question" entry (file "~/org/all.org") "** Q from %^{Who} re: %^{Subject}") ("i" "Random Item" entry (file "~/org/all.org") "** %^{What}")) (quote (org-clock-continuously t)) (quote (org-clock-into-drawer t)) (quote (org-default-notes-file "~/org/TODO.org")) (quote (org-default-priority 65)) (quote (org-log-done (quote time))) (quote (org-stuck-projects (quote ("+PROJECT/-MAYBE-DONE" ("TODO" "NBIJ" "Waiting" "NEXT" "NEXTACTION") nil "")))) (quote (perl-indent-level 8)) (quote (post-fixed-signature-source "~/.signature")) (quote (post-should-prompt-for-attachment (quote Never))) (quote (require-final-newline t)) (quote (safe-local-variable-values (quote ((mangle-whitespace . t) (rm-trailing-spaces . t))))) (quote (scroll-bar-mode -1)) (quote (send-mail-function (quote sendmail-send-it))) (quote (setq c-basic-offset)) (quote (shell-file-name "/bin/bash")) (quote (show-paren-mode 1)) (quote (spell-command "ispell")) (quote (split-window-preferred-function nil)) (quote (standard-indent 8)) (quote (uniquify-buffer-name-style (quote post-forward) nil (uniquify))) (quote (uniquify-min-dir-content 2)) (quote (vc-follow-symlinks nil)) (quote (w3m-use-cookies t)) (quote (wg-morph-on nil)) (quote (win-switch-idle-time 1.25)) (quote (x-select-enable-clipboard t))))
+ '(org-capture-templates
+   (quote
+    (("R" "Autofill RT ticket" entry
+      (file "~/orgmode/todo.org")
+      "** TODO [#A] %?")
+     ("e" "Emacs" entry
+      (file "~/orgmode/emacs.org")
+      "** TODO RT #%^{Number} -- %^{Subject}")
+     ("t" "TODO" entry
+      (file "~/orgmode/todo.org")
+      "** TODO %^{TODO}")
+     ("q" "Question" entry
+      (file "~/orgmode/TODO.org")
+      "** Q from %^{Who} re: %^{Subject}")
+     ("i" "Random Item" entry
+      (file "~/orgmode/TODO.org")
+      "** %^{What}"))))
+ '(org-clock-continuously t)
+ '(org-clock-into-drawer t)
  '(org-default-notes-file "~/orgmode/TODO.org")
- '(org-log-into-drawer t))
+ '(org-habit-show-all-today t)
+ '(org-log-into-drawer t)
+ '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
+ '(org-default-priority 65)
+ '(org-log-done (quote time))
+ '(org-stuck-projects
+   (quote
+    ("+PROJECT/-MAYBE-DONE"
+     ("TODO" "NBIJ" "Waiting" "NEXT" "NEXTACTION")
+     nil "")))
+ '(perl-indent-level 8)
+ '(post-should-prompt-for-attachment (quote Never))
+ '(require-final-newline t)
+ '(safe-local-variable-values (quote ((mangle-whitespace . t) (rm-trailing-spaces . t))))
+ '(scroll-bar-mode -1)
+ '(send-mail-function (quote sendmail-send-it))
+ '(setq c-basic-offset)
+ '(show-paren-mode 1)
+ '(spell-command "ispell")
+ '(split-window-preferred-function nil)
+ '(standard-indent 8)
+ '(uniquify-buffer-name-style (quote post-forward) nil (uniquify))
+ '(uniquify-min-dir-content 2)
+ '(vc-follow-symlinks nil)
+ '(w3m-use-cookies t)
+ '(wg-morph-on nil)
+ '(win-switch-idle-time 1.25)
+ '(x-select-enable-clipboard t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
