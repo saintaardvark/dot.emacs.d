00f9185b15b96df857d279439c2b55ec42ea4992
Saint Aardvark the Carpeted <aardvark@zombie.saintaardvarkthecarpeted.com>
2015-07-24 17:46:22 -0700

    x-hugh-helm.

---
+++ x-hugh-helm.el
@@ -0,0 +1,17 @@
+;; Break out all helm stuff into one place.
+
+(if (eq window-system nil)
+    (setq x-hugh-helm-enabled nil)
+  (setq x-hugh-helm-enabled t))
+
+(if x-hugh-helm-enabled
+    (progn
+      (require 'helm-config   nil 'noerror)
+      (helm-mode 1)
+      (global-set-key (kbd "M-x")     'helm-M-x)
+      (global-set-key (kbd "M-y")     'helm-show-kill-ring)
+      (global-set-key (kbd "C-x b")   'helm-mini)
+      (global-set-key (kbd "C-x C-f") 'helm-find-files)
+      (setq helm-ff-newfile-prompt-p nil)))
+
+(provide 'x-hugh-helm)
--- x-hugh-init.el
+++ x-hugh-init.el
@@ -22,6 +22,7 @@
 (require 'x-hugh-org)
 (require 'x-hugh-cfengine)
 (require 'x-hugh-settings)
+(require 'x-hugh-helm)
 (require 'x-hugh-reference)
 (require 'x-hugh-modes)
 (require 'x-hugh-keymap)
