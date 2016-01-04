8c31d208333b09d888a5b38bebe990fe28ff130f
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2016-01-04 06:33:54 -0800

    Fix binding for this.

--- x-hugh-keymap.el
+++ x-hugh-keymap.el
@@ -6,7 +6,7 @@
 
 ;; open confluence page
 (global-set-key "\C-xs"		'replace-region-command-output)
-(global-set-key "\C-cb"		'x-hugh-chronicle-new-blog-entry)
+(global-set-key "\C-cb"		'x-hugh-jekyll-new-blog-entry)
 (global-set-key "\C-ck"		'compile)
 (global-set-key "\C-cm"		'magit-status)
 (global-set-key "\C-cn"		'notmuch-search)
