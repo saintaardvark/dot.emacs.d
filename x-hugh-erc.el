622836bcd946729e0076314ff90ccb33b06ca16b
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2016-01-07 05:59:32 -0800

    Not using erc anymore.

--- x-hugh-erc.el
+++ x-hugh-erc.el
@@ -8,10 +8,11 @@
 (setq erc-hide-list (quote ("JOIN" "PART" "QUIT")))
 (setq erc-nick "SaintAardvark")
 
-(setq erc-autojoin-channels-alist '(("irc.freenode.net"
-				     "#emacs"
-				     "#chef"
-				     )))
+;; Moved to Weechat...but I'll keep this for reference.
+;; (setq erc-autojoin-channels-alist '(("irc.freenode.net"
+;; 				     "#emacs"
+;; 				     "#chef"
+;; 				     )))
 
 (provide 'x-hugh-erc)
 ;;; x-hugh-erc ends here
