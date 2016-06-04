98d8b6f076ed5cc06d025fdb6b735de55910d5f5
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2016-06-04 07:17:23 -0700

    ARGHHH

---
+++ x-hugh-projectile.el
@@ -0,0 +1,37 @@
+;; x-hugh-projectile --- start here!
+
+;;; Commentary:
+
+;;; Code:
+
+
+;; (add-hook 'chef-mode-hook 'projectile-mode)
+(projectile-global-mode)
+
+(projectile-register-project-type
+ 'chef
+ '("Berksfile")           ; marker files
+ "bundle exec rails server"             ; compile command
+ "chef exec rspec"                      ; test command
+ "kitchen test")                        ; run command
+
+;; Copy-pasta...there must be a better way to do this.
+
+(defun x-hugh-projectile-test-suffix (project-type)
+  "Find default test files suffix based on PROJECT-TYPE."
+  (cond
+   ((member project-type '(emacs-cask)) "-test")
+   ((member project-type '(rails-rspec ruby-rspec)) "_spec")
+   ((member project-type '(rails-test ruby-test lein-test boot-clj go)) "_test")
+   ((member project-type '(scons)) "test")
+   ((member project-type '(maven symfony)) "Test")
+   ((member project-type '(gradle gradlew grails)) "Spec")
+   ;; And now chef
+   ((member project-type '(chef)) "_spec")))
+
+(setq projectile-test-suffix-function 'x-hugh-projectile-test-suffix)
+
+(global-set-key (kbd "C-c p p") 'helm-projectile-switch-project)
+
+(provide 'x-hugh-projectile)
+;;; x-hugh-projectile ends here
