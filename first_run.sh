a4a80cd4b426ce28c6a0e31456df3d4b32174137
Hugh Brown <hugh@opendns.com>
2016-09-25 15:54:11 -0700

    Account for OSX and Emacs.app.

--- first_run.sh
+++ first_run.sh
@@ -6,5 +6,13 @@ if [ -d ~/.cask ] ; then
 else
 	git clone https://github.com/cask/cask.git ~/.cask
 fi
+
 cd ~/.emacs.d
+
+# Necessary on OSX; see
+# http://stackoverflow.com/questions/18330954/using-cask-with-emacs-app
+if [ -f /Applications/Emacs.app/Contents/MacOS/Emacs ] ; then
+	export EMACS=/Applications/Emacs.app/Contents/MacOS/Emacs
+fi
+
 ~/.cask/bin/cask
