f43cc73d70904c6301fe36718d27ba7781e32c82
Hugh Brown <hugh@opendns.com>
2016-09-25 13:17:17 -0700

    Add x-hugh-yasnippet.

---
+++ x-hugh-yasnippet.el
@@ -0,0 +1,12 @@
+;;; x-hugh-yasnippet --- Provide yasnippet
+
+;;; Commentary:
+
+;;; Code:
+(require 'yasnippet nil 'noerror)
+(yas-global-mode 1)
+
+(yas-load-directory "/Users/hubrown/.emacs.d/snippets")
+
+(provide 'x-hugh-yasnippet)
+;;; x-hugh-yasnippet ends here
