a0aee076df6d797e6930287f8a803ecbcaef256c
Hugh Brown <hugh@opendns.com>
2016-11-19 07:54:55 -0800

    Add a macro for adding smart-parens mode to a mode hook
    Takes a single value now; next step is to take a list.

--- x-hugh-smartparens.el
+++ x-hugh-smartparens.el
@@ -4,7 +4,15 @@
 
 ;;; Code:
 
-;; smartparens
+;; FIXME: This should be a list, not a sngle mode
+(defmacro x-hugh-sp-add-hook-to-mode (mode)
+  "Ensure that smart-parens mode is added to (MODE)-hook."
+  (let ((hooksymbol (intern (concat mode "-mode-hook"))))
+    `(add-hook ',hooksymbol #'smartparens-mode)
+  ))
+
+(x-hugh-sp-add-hook-to-mode "emacs-lisp")
+
 (add-hook 'emacs-lisp-mode-hook #'smartparens-mode)
 (add-hook 'json-mode-hook #'smartparens-mode)
 (add-hook 'ruby-mode-hook #'smartparens-mode)
