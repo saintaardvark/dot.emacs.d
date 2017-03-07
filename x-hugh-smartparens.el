59d2356e411379b445e3b16f26c862ad562aaacd
Hugh Brown <hugh@opendns.com>
2017-03-07 08:31:53 -0800

    Use macro for managing smartparens and hooks

--- x-hugh-smartparens.el
+++ x-hugh-smartparens.el
@@ -5,13 +5,20 @@
 ;;; Code:
 
 ;; FIXME: This should be a list, not a sngle mode
-(defmacro x-hugh-sp-add-hook-to-mode (mode)
-  "Ensure that smart-parens mode is added to (MODE)-hook."
-  (let ((hooksymbol (intern (concat mode "-mode-hook"))))
-    `(add-hook ',hooksymbol #'smartparens-mode)
-  ))
+(defmacro x-hugh-sp-add-hook-to-modes (modes)
+  "Ensure that smart-parens mode is added to each of (MODES) mode-hook."
+  (dolist (mode modes)
+    (let ((hooksymbol (intern (concat mode "-mode-hook"))))
+      `(add-hook ',hooksymbol #'smartparens-mode))))
 
-(x-hugh-sp-add-hook-to-mode "emacs-lisp")
+(x-hugh-sp-add-hook-to-modes ("emacs-lisp"
+                              "go"
+                              "json"
+                              "ruby"
+                              "python"
+                              "shell"
+                              "toml"
+                              "yaml"))
 
 (add-hook 'emacs-lisp-mode-hook #'smartparens-mode)
 (add-hook 'json-mode-hook #'smartparens-mode)
