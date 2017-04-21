e3846e03cc37bc6d10a72b7ccbe4169f150af081
Hugh Brown <hugh@opendns.com>
2017-04-21 08:58:47 -0700

    Don't quote the add-hook statement
    If you quote it, it'll never get eval'd!

--- x-hugh-smartparens.el
+++ x-hugh-smartparens.el
@@ -4,12 +4,12 @@
 
 ;;; Code:
 
-;; FIXME: This should be a list, not a sngle mode
+
 (defmacro x-hugh-sp-add-hook-to-modes (modes)
   "Ensure that smart-parens mode is added to each of (MODES) mode-hook."
   (dolist (mode modes)
     (let ((hooksymbol (intern (concat mode "-mode-hook"))))
-      `(add-hook ',hooksymbol #'smartparens-mode))))
+      (add-hook hooksymbol #'smartparens-mode))))
 
 (x-hugh-sp-add-hook-to-modes ("emacs-lisp"
                               "go"
@@ -19,15 +19,6 @@
                               "shell"
                               "toml"
                               "yaml"))
-
-(add-hook 'emacs-lisp-mode-hook #'smartparens-mode)
-(add-hook 'json-mode-hook #'smartparens-mode)
-(add-hook 'ruby-mode-hook #'smartparens-mode)
-(add-hook 'python-mode-hook #'smartparens-mode)
-(add-hook 'shell-mode-hook #'smartparens-mode)
-(add-hook 'yaml-mode-hook #'smartparens-mode)
-(add-hook 'toml-mode-hook #'smartparens-mode)
-(add-hook 'go-mode-hook #'smartparens-mode)
 
 (provide 'x-hugh-smartparens)
 ;;; x-hugh-smartparens ends here
