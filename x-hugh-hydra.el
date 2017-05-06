2b9a15fd7a13da5cf7ee668cc0d1b51fddf6e63c
Hugh Brown <hugh@opendns.com>
2017-05-05 19:36:34 -0700

    Add hydra for navigation

--- x-hugh-hydra.el
+++ x-hugh-hydra.el
@@ -62,6 +62,54 @@ _v_ariable       _u_ser-option
   ("c" x-hugh-copy-and-comment-region "Copy and comment region")
   ("l" x-hugh-copy-and-comment-line "Copy and comment line"))
 
+;; http://kitchingroup.cheme.cmu.edu/blog/2015/09/28/A-cursor-goto-hydra-for-emacs/
+(defhydra hydra-goto (:color blue :hint nil)
+  "
+Goto:
+^Char^              ^Word^                ^org^                    ^search^
+^^^^^^^^---------------------------------------------------------------------------
+_c_: char           _w_: word by char     _h_: headline in buffer  _o_: helm-occur
+_C_: 2 chars        _W_: some word        _a_: heading in agenda   _p_: helm-swiper
+_L_: char in line   _s_: subword by char  _q_: swoop org buffers   _f_: search forward
+^  ^                _S_: some subword     ^ ^                      _b_: search backward
+-----------------------------------------------------------------------------------
+_B_: helm-buffers       _g_: avy-goto-line
+_m_: helm-mini          _i_: ace-window
+_R_: helm-recentf
+
+_n_: Navigate           _._: mark position _/_: jump to mark
+"
+  ("c" avy-goto-char)
+  ("C" avy-goto-char-2)
+  ("L" avy-goto-char-in-line)
+  ("w" avy-goto-word-1)
+  ;; jump to beginning of some word
+  ("W" avy-goto-word-0)
+  ;; jump to subword starting with a char
+  ("s" avy-goto-subword-1)
+  ;; jump to some subword
+  ("S" avy-goto-subword-0)
+
+  ("g" avy-goto-line)
+  ("i" ace-window)
+
+  ("h" helm-org-headlines)
+  ("a" helm-org-agenda-files-headings)
+  ("q" helm-multi-swoop-org)
+
+  ("o" helm-occur)
+  ("p" swiper-helm)
+
+  ("f" isearch-forward)
+  ("b" isearch-backward)
+
+  ("." org-mark-ring-push :color red)
+  ("/" org-mark-ring-goto :color blue)
+  ("B" helm-buffers-list)
+  ("m" helm-mini)
+  ("R" helm-recentf)
+  ("n" hydra-navigate/body))
+
 (defhydra hydra-puppet (:color blue :hint nil)
   "
 _a_ccesscontrol
