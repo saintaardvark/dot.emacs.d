38edeeac0bc1761c713e7c0e8e2b366fa5e77e1c
Hugh Brown <hugh@opendns.com>
2017-05-23 20:31:12 -0700

    Do the right thing w/smartparens and brackets in Go

--- x-hugh-smartparens.el
+++ x-hugh-smartparens.el
@@ -6,6 +6,9 @@
 
 ;; FIXME: This should be made more generic:
 ;; (x-hugh-add-hook-to-modes "smartparens" ("this" "that" "the other" ...))
+
+(require 'smartparens)
+
 (defmacro x-hugh-sp-add-hook-to-modes (modes)
   "Ensure that smart-parens mode is added to each of (MODES) mode-hook."
   (dolist (mode modes)
@@ -14,6 +17,7 @@
 
 (x-hugh-sp-add-hook-to-modes ("emacs-lisp"
                               "go"
+                              "groovy"
                               "jinja2"
                               "json"
                               "markdown"
@@ -23,6 +27,16 @@
                               "terraform"
                               "toml"
                               "yaml"))
+
+;; See https://github.com/Fuco1/smartparens/wiki/Permissions
+(sp-local-pair 'go-mode "{" nil :post-handlers '((x-hugh-create-newline-and-enter-sexp "RET")))
+(sp-local-pair 'go-mode "(" nil :post-handlers '((x-hugh-create-newline-and-enter-sexp "RET")))
+(defun x-hugh-create-newline-and-enter-sexp (&rest _ignored)
+  "Open a new brace or bracket expression, with relevant newlines and indent."
+  (newline)
+  (indent-according-to-mode)
+  (forward-line -1)
+  (indent-according-to-mode))
 
 (provide 'x-hugh-smartparens)
 ;;; x-hugh-smartparens ends here
