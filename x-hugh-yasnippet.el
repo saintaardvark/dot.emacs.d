140422e9e166365625867aa3e269b96f1ada4d2e
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2017-12-13 18:02:00 -0800

    Future work...
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- x-hugh-yasnippet.el
+++ x-hugh-yasnippet.el
@@ -25,5 +25,27 @@
 
 (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
 
+;; https://www.emacswiki.org/emacs/Yasnippet#toc4
+;; not yet working.
+(defun shk-yas/helm-prompt (prompt choices &optional display-fn)
+    "Use helm to select a snippet. Put this into `yas-prompt-functions.'"
+    (interactive)
+    (setq display-fn (or display-fn 'identity))
+    (if (require 'helm-config)
+        (let (tmpsource cands result rmap)
+          (setq cands (mapcar (lambda (x) (funcall display-fn x)) choices))
+          (setq rmap (mapcar (lambda (x) (cons (funcall display-fn x) x)) choices))
+          (setq tmpsource
+                (list
+                 (cons 'name prompt)
+                 (cons 'candidates cands)
+                 '(action . (("Expand" . (lambda (selection) selection))))
+                 ))
+          (setq result (helm-other-buffer '(tmpsource) "*helm-select-yasnippet"))
+          (if (null result)
+              (signal 'quit "user quit!")
+            (cdr (assoc result rmap))))
+      nil))
+
 (provide 'x-hugh-yasnippet)
 ;;; x-hugh-yasnippet ends here
