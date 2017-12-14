0297299387824f39bc3476777e49eb728e961ece
Hugh Brown <hugh@opendns.com>
2017-12-13 21:16:39 -0800

    Add melpa-via-SSL
    Signed-off-by: Hugh Brown <hugh@opendns.com>

--- x-hugh-init.el
+++ x-hugh-init.el
@@ -33,9 +33,15 @@
 (load custom-file 'noerror)
 
 ;; Add melpa
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 ;; Set up path
 (if (file-exists-p "/usr/local/bin")
