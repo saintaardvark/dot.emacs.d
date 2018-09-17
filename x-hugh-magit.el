b54aa5660d097b7e7cdce8ba892b2bef3599c64a
Hugh Brown <hugh@opendns.com>
2018-09-17 08:42:10 -0700

    Be a little more clear about disabling magithub
    Being prompted to auth when working in private repo is just annoying.
    
    Signed-off-by: Hugh Brown <hugh@opendns.com>

--- x-hugh-magit.el
+++ x-hugh-magit.el
@@ -17,10 +17,11 @@
 ;; ;; (require 'magit nil 'noerror)
 
 (require 'magit)
+
 ;; Turning this off -- refreshing CI status every time is very, very slow.
-(require 'magithub)
-(require 'magit-gh-pulls)
-(add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
+;; (require 'magithub)
+;; (require 'magit-gh-pulls)
+;; (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
 
 ;; From https://magit.vc/manual/magit/Performance.html#Performance
 (setq magit-refresh-status-buffer nil)
