03793899c82c39c2ea59da5c860fafb878664470
Hugh Brown <hugh@opendns.com>
2018-11-13 11:16:40 -0800

    use-package for helm
    Signed-off-by: Hugh Brown <hugh@opendns.com>

--- x-hugh-helm.el
+++ x-hugh-helm.el
@@ -9,16 +9,18 @@
   (setq x-hugh-helm-enabled t))
 
 (if x-hugh-helm-enabled
-    (progn
-      (require 'helm-config   nil 'noerror)
-      (helm-mode 1)
-      (global-set-key (kbd "M-x")     'helm-M-x)
-      (global-set-key (kbd "M-y")     'helm-show-kill-ring)
-      (global-set-key (kbd "C-x b")   'helm-mini)
-      (global-set-key (kbd "C-x C-f") 'helm-find-files)
-      ;; helm-do-grep is better.
-      ;; (global-set-key "\C-cif"	'helm-do-grep)
-      (setq helm-ff-newfile-prompt-p nil)))
+    (use-package helm
+      :config
+      (progn
+        (helm-mode 1)
+        (global-set-key (kbd "M-x")     'helm-M-x)
+        (global-set-key (kbd "M-y")     'helm-show-kill-ring)
+        (global-set-key (kbd "C-x b")   'helm-mini)
+        (global-set-key (kbd "C-x C-f") 'helm-find-files)
+        ;; helm-do-grep is better.
+        ;; (global-set-key "\C-cif"	'helm-do-grep)
+        (setq helm-ff-newfile-prompt-p nil))
+      (use-package helm-config)))
 
 (provide 'x-hugh-helm)
 ;;; x-hugh-helm.el ends here
