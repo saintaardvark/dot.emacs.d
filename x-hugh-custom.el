d8b2ee6234ab41eb5076a3761e0f413a2144228f
Hugh Brown <hubrown@cisco.com>
2019-01-10 08:13:12 -0800

    Add org settings
    Signed-off-by: Hugh Brown <hubrown@cisco.com>

--- x-hugh-custom.el
+++ x-hugh-custom.el
@@ -87,6 +87,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fixmee-notice-face ((t (:background "gray88" :foreground "dark red" :underline nil :slant italic :weight bold)))))
+ '(fixmee-notice-face ((t (:background "gray88" :foreground "dark red" :underline nil :slant italic :weight bold))))
+ '(org-done ((t (:foreground "PaleGreen" :weight normal :strike-through t))))
+ '(org-headline-done ((((class color) (min-colors 16) (background dark)) (:foreground "LightSalmon" :strike-through t))))
+ '(org-level-1 ((t (:inherit nil :foreground "#cb4b16" :height 1.3))))
+ '(org-level-2 ((t (:inherit nil :foreground "#859900" :height 1.2))))
+ '(org-level-3 ((t (:inherit nil :foreground "#268bd2" :height 1.15))))
+ '(org-level-4 ((t (:inherit nil :foreground "#b58900" :height 1.1))))
+ '(org-level-5 ((t (:inherit nil :foreground "#2aa198"))))
+ '(org-level-6 ((t (:inherit nil :foreground "#859900"))))
+ '(org-level-7 ((t (:inherit nil :foreground "#dc322f"))))
+ '(org-level-8 ((t (:inherit nil :foreground "#268bd2")))))
 
 ;;; x-hugh-custom ends here.
