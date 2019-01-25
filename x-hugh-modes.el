02d22c760339179669e1a558b0dbc04a4f35a787
Hugh Brown <hubrown@cisco.com>
2019-01-25 11:25:41 -0800

    Alphabetize/re-org
    Signed-off-by: Hugh Brown <hubrown@cisco.com>

--- x-hugh-modes.el
+++ x-hugh-modes.el
@@ -14,6 +14,51 @@
 ;;   :custom (highlight-indent-guides-method 'character)
 ;;   :config (add-hook 'prog-mode-hook 'highlight-indent-guides-mode))
 
+
+;; I'm gonna break this habit if it kills me
+(use-package annoying-arrows-mode
+  :config (global-annoying-arrows-mode))
+;; FIXME: Pretty sure there's a better way to add hooks here...
+;; For ANSI colourization in compilation buffers.
+;; https://stackoverflow.com/questions/13397737/ansi-coloring-in-compilation-mode/20788581#20788581
+(defun my-colorize-compilation-buffer ()
+  (when (eq major-mode 'compilation-mode)
+    (ansi-color-apply-on-region compilation-filter-start (point-max))))
+
+(use-package ansi-color
+  :custom (add-hook 'compilation-filter-hook 'my-colorize-compilation-buffer))
+(use-package apache-mode
+  :mode (("\\.htaccess\\'" . apache-mode)
+         ("httpd\\.conf\\'"  . apache-mode)
+         ("srm\\.conf\\'"    . apache-mode)
+         ("access\\.conf\\'" . apache-mode)
+         ("sites-\\(available\\|enabled\\)/" . apache-mode)))
+(use-package browse-kill-ring)
+(use-package boxquote)
+;; FIXME: Think I can use custom here
+(use-package dired
+  :config
+  (setq dired-dwim-target t)
+  (setq dired-recursive-copies 'always)
+  (setq ddired-recursive-deletes 'top))
+(use-package flycheck
+  :config (add-hook 'after-init-hook #'global-flycheck-mode)
+  :custom ((flycheck-check-syntax-automatically (quote (save idle-change)))
+           (flycheck-flake8-maximum-line-length 9990)
+           (flycheck-idle-change-delay 2)
+           (flycheck-keymap-prefix ".")))
+(use-package linum)
+(use-package midnight
+  :custom ((midnight-mode t)
+           (clean-buffer-list-delay-general 1)
+           (clean-buffer-list-kill-never-buffer-names (quote ("*scratch*" "*Messages*" "*server*" ".\\*\\.org$")))))
+;; Because it's fun.
+;; FIXME: do I need to enable using :config?
+(use-package nyan-mode
+  :config (nyan-mode))
+(use-package paren
+  :config (setq show-paren-mode 1))
+
 ;; Use post for Mutt.
 (use-package post
   :custom ((post-email-address "aardvark@saintaardvarkthecarpeted.com")
@@ -29,29 +74,22 @@
   ;; which is seriously fucking with my use of Mutt and having an Emacs
   ;; daemon hanging around all the time.  This is new behaviour as of
   ;; 24.2; not sure what is different.  This is ugly but it works.
-  (fset 'post-finish 'server-edit)
+                                        ; (fset 'post-finish 'server-edit)
   :mode ("mutt.*$" . post-mode)
   :hook (abbrev-mode smartparens-mode)
   )
+;; Load ssh.
+;; FIXME: Don't have this anymore :-(
+(use-package ssh)
 
 ;; Use scp for tramp.
 (use-package tramp
   :config
   (setq tramp-default-method "scp"))
 
-;; Load ssh.
-(use-package ssh)
-
 ;; Shell script stuff
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
-;; Random
-(use-package boxquote)
-(use-package linum)
-(use-package midnight
-  :custom ((midnight-mode t)
-           (clean-buffer-list-delay-general 1)
-           (clean-buffer-list-kill-never-buffer-names (quote ("*scratch*" "*Messages*" "*server*" ".\\*\\.org$")))))
 (use-package uniquify
   :custom ((uniquify-buffer-name-style 'post-forward nil (uniquify))
            (uniquify-min-dir-content 2)))
@@ -66,44 +104,7 @@
 ;; Diff mode
 (add-hook 'diff-mode 'font-lock-mode)
 
-(use-package browse-kill-ring)
-
-;; Apache mode.
-
-(use-package apache-mode
-  :mode (("\\.htaccess\\'" . apache-mode)
-         ("httpd\\.conf\\'"  . apache-mode)
-         ("srm\\.conf\\'"    . apache-mode)
-         ("access\\.conf\\'" . apache-mode)
-         ("sites-\\(available\\|enabled\\)/" . apache-mode)))
-
-;; Because it's fun.
-;; FIXME: do I need to enable using :config?
-(use-package nyan-mode
-  :config (nyan-mode))
-
-;; I'm gonna break this habit if it kills me
-(use-package annoying-arrows-mode
-  :config (global-annoying-arrows-mode))
-
-;; Flycheck
-(use-package flycheck
-  :config (add-hook 'after-init-hook #'global-flycheck-mode)
-  :custom ((flycheck-check-syntax-automatically (quote (save idle-change)))
-           (flycheck-flake8-maximum-line-length 9990)
-           (flycheck-idle-change-delay 2)
-           (flycheck-keymap-prefix ".")))
-
 (use-package yasnippet)
-
-;; For ANSI colourization in compilation buffers.
-;; https://stackoverflow.com/questions/13397737/ansi-coloring-in-compilation-mode/20788581#20788581
-(defun my-colorize-compilation-buffer ()
-  (when (eq major-mode 'compilation-mode)
-    (ansi-color-apply-on-region compilation-filter-start (point-max))))
-;; FIXME: Pretty sure there's a better way to add hooks here...
-(use-package ansi-color
-  :custom (add-hook 'compilation-filter-hook 'my-colorize-compilation-buffer))
 
 ;; Man, I always forget about winner-mode...
 (winner-mode 1)
@@ -113,13 +114,6 @@
 
 ;; Jenkinsfile
 (add-to-list 'auto-mode-alist '("Jenkinsfile\\'" . groovy-mode))
-
-;; Dired
-(use-package dired
-  :config
-  (setq dired-dwim-target t)
-  (setq dired-recursive-copies 'always)
-  (setq ddired-recursive-deletes 'top))
 
 ;; Noice!
 ;; https://emacs.stackexchange.com/questions/202/close-all-dired-buffers
@@ -131,9 +125,6 @@
                  )))
 
 (add-hook 'ibuffer-mode-hook #'(ibuffer-switch-to-saved-filter-groups "Default"))
-
-(use-package paren
-  :config (setq show-paren-mode 1))
 
 (provide 'x-hugh-modes)
 ;;; x-hugh-modes ends here
