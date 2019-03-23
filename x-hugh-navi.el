bf2667fd79f2ab1237c9b492119936bc92fa7d63
Hugh Brown <hubrown@cisco.com>
2019-03-23 11:55:27 -0700

    Change navi single/double click to navigate by char/line

--- x-hugh-navi.el
+++ x-hugh-navi.el
@@ -26,12 +26,23 @@ names here.")
   (let ((f (intern (format "avy-goto-%s" (car navi-list-of-navigation-methods)))))
     (call-interactively f)))
 
-;; FIXME: This all talks about clicks, when in fact it's about
+;; Note: This all talks about clicks, when in fact it's about
 ;; toggling which function gets called.
+
+;; Originally, I'd set up navi so that a single click would navigate
+;; somewhere, and a double click would change the method of navigation
+;; (from char to word to line). :
+;; (defvar navi-single-click-cmd 'navi-call-navigation-method)
+;; (defvar navi-double-click-cmd 'navi-rotate-method)
+;;
+;; However, I've come to realize that I mainly navigate using either
+;; character or line, Thus, single click navigates by character;
+;; double click navigates by line.
+(defvar navi-single-click-cmd 'avy-goto-char)
+(defvar navi-double-click-cmd 'avy-goto-line)
+
 ;; FIXME: We're using double-click-time here; should set a variable for this.
 ;; https://stackoverflow.com/questions/4923723/emacs-how-to-bind-a-key-tapped-twice
-(defvar navi-single-click-cmd 'navi-call-navigation-method)
-(defvar navi-double-click-cmd 'navi-rotate-method)
 (defvar navi-click-timer nil
   "Pending single-click event.")
 
@@ -48,12 +59,22 @@ names here.")
                                       nil
                                       'navi-call-single-click))))
 
+;; FIXME: I'd really like it if I could do the same kind of
+;; do-one-thing-unless-i-repeat-the-command approach for the control
+;; key:
+
+;; Ctrl [whatever] - usual C-[whatever]
+;; Ctrl-Ctrl - Meta
+;;
+;; Would save me from alt/option pinky.  Wonder if that's possible
+;; from w/in emacs or need to go to OS.  Maybe C-c would do it?  DOH
+;; -- Keep forgetting that C-[ does this!  Not quite as convenient,
+;; but will do.
+
 (defun navi-call-single-click ()
   "Spawn the single click."
   (setq navi-click-timer nil)
   (call-interactively navi-single-click-cmd))
-
-;; (navi-call-navigation-method)
 
 (provide 'x-hugh-navi)
 ;;; x-hugh-navi.el ends here
