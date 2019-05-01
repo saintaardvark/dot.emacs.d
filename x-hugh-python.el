520de7a41f21da4193d1a4f68332fbe14cc10105
Hugh Brown <hubrown@cisco.com>
2019-05-01 08:24:04 -0700

    Add function to toggle highlight-indentation-mode

--- x-hugh-python.el
+++ x-hugh-python.el
@@ -17,6 +17,19 @@
 ;; I'm not liking how this looks
 ;; :config (add-hook 'prog-mode-hook 'highlight-indent-guides-mode))
 
+(defun x-hugh-highlight-indentation-mode-toggle ()
+  "Toggle whether highlight-indentation-mode is enabled.
+
+If highlight-indentation-mode is bound (as in, it is a function)
+and highlight-indentation-mode (the variable) is true, turn off;
+else, turn on."
+  (interactive)
+  (if (and (boundp 'highlight-indentation-mode)
+	   (eq highlight-indentation-mode t))
+      (highlight-indentation-mode -1)
+    (highlight-indentation-mode)))
+(x-hugh-highlight-indentation-mode-toggle)
+
 (use-package python
   :custom (python-indent-guess-indent-offset t))
 
