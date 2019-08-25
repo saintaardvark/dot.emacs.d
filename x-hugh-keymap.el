f804b32e67f079f0d0b0a2945efb3caf3c2cca50
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2019-08-24 17:23:37 -0700

    Add keybinding for er/expand-region
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- x-hugh-keymap.el
+++ x-hugh-keymap.el
@@ -80,6 +80,8 @@
 (global-set-key (kbd "C-c y") 'hydra-python/body)
 (global-set-key (kbd "C-c z") 'hydra-zoom/body)
 
+;; Oooh, expand-region!
+(global-set-key (kbd "C-=") 'er/expand-region)
 
 ;; For some reason, markdown mode grabs this binding.  I *REALLY* want
 ;; it back.  I don't know why this isn't working.
