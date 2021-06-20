35363b29f4397ab52d8f5bc4dbfdab7025930d3a
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2021-06-19 18:13:54 -0700

    Fix bug in jcs-kill-a-buffer
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- x-hugh-random.el
+++ x-hugh-random.el
@@ -80,9 +80,7 @@
 
 ;; From
 ;; http://pragmaticemacs.com/emacs/dont-kill-buffer-kill-this-buffer-instead/
-;; FIXME: Help for kill-this-buffer says: "This command can be
-;; reliably invoked only from the menu bar,otherwise it could decide
-;; to silently do nothing." I seem to be encountering that now. :-(
+;; but adapted to not use `kill-this-buffer`.
 (defun jcs-kill-a-buffer (askp)
   "Just kill this damn buffer! If ASKP provided, ask which buffer to kill."
   (interactive "P")
@@ -90,7 +88,7 @@
       (kill-buffer (funcall completing-read-function
                             "Kill buffer: "
                             (mapcar #'buffer-name (buffer-list))))
-    (kill-this-buffer)))
+    (kill-buffer (current-buffer))))
 
 ;; Chrome extension for editing stuff in Emacs
 (use-package edit-server
