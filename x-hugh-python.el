2af345b450ac91ee1afbbc449d3f8549ab2d482a
Hugh Brown <hubrown@cisco.com>
2021-11-10 13:45:37 -0800

    Trying out pyright as LSP server for Python.  Seems good so far

--- x-hugh-python.el
+++ x-hugh-python.el
@@ -7,17 +7,30 @@
 
 ;; FIXME: Turn off auto-complete & auto-composition in *at least*
 ;; Python, possibly all modes.
-(use-package elpy
-  ;; The default list, but minus highlight-indentation.
-  ;; Giving highlight-indentationo-guides a try.
-  :custom (elpy-modules '(elpy-module-sane-defaults
-			  elpy-module-company
-			  elpy-module-eldoc
-			  elpy-module-flymake
-			  elpy-module-pyvenv
-			  elpy-module-yasnippet
-			  elpy-module-django))
-  :config (elpy-enable))
+;; (use-package elpy
+;;   ;; The default list, but minus highlight-indentation.
+;;   ;; Giving highlight-indentationo-guides a try.
+;;   :custom (elpy-modules '(elpy-module-sane-defaults
+;; 			  elpy-module-company
+;; 			  elpy-module-eldoc
+;; 			  elpy-module-pyvenv
+;; 			  elpy-module-yasnippet
+;; 			  elpy-module-django))
+;;   :config (elpy-enable))
+
+(use-package dash
+  :ensure t)
+(dash-unload-function)
+
+(-map (lambda (num) (* num num)) '(1 2 3 4))
+(funcall (-compose #'- #'1+ #'+) 1 2 3)
+
+
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+			 (require 'lsp-pyright)
+			 (lsp)))) 	;or lsp-deferred
 
 (defun x-hugh-highlight-indentation-mode-toggle ()
   "Toggle whether highlight-indentation-mode is enabled.
