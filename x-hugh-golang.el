f58a3e24ac708d0b872360d2a6e2d384781bbf1f
Hugh Brown <hubrown@cisco.com>
2021-11-10 13:40:54 -0800

    Update x-hugh-golang.el
    - flycheck settings
    - compile command
    - GO111MODULE=auto

--- x-hugh-golang.el
+++ x-hugh-golang.el
@@ -6,8 +6,22 @@
 
 (exec-path-from-shell-copy-env "GOPATH")
 (exec-path-from-shell-copy-env "GOROOT")
+(setenv "GO111MODULE" "auto")		; Will this fix gopls borking in streamline-ssh?
 
 (go-eldoc-setup)
+
+;; FIXME: Flycheck + go vet has a very annoying error in golang mode
+;; that doesn't seem to have been fixed.  Full details at
+;; https://github.com/flycheck/flycheck/issues/1659, but in the
+;; meantime you'll want to turn off flycheck in golang.
+
+;; FIXME: One more problem with flycheck: seems to break when checking
+;; imports when lsp is turned on.  Maybe relevant:
+;; https://github.com/emacs-lsp/lsp-mode/issues/2207
+
+;; FIXME: gocode not on laptop, causing flashing when it tries to run.
+;; Probably an autocomplete/company thing.  However, looks like it's
+;; no longer maintained: https://github.com/nsf/gocode :blobfish:
 
 ;; FIXME: Figure out how to get goimports running here
 (defun go-mode-setup ()
@@ -15,8 +29,9 @@
   (add-hook 'before-save-hook 'gofmt-before-save)
   (local-set-key (kbd "M-.") 'godef-jump)
   (company-mode)
+  (setq flycheck-checkers (remove "go-vet" flycheck-checkers))
   (set (make-local-variable 'company-backends) '(company-go))
-  (setq compile-command "go build -v && go test -v && go vet && golint && errcheck")
+  (setq compile-command "")
   (setq indent-tabs-mode 1)
   (setq tab-width 4))
 
