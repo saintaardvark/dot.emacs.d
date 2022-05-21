ae6d61840607eb5fcbe81fd8f2dd6cd73503ac4e
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2022-05-21 11:28:38 -0700

    Install highlight-indents-guide, remove unneeded function
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- lisp/x-hugh-python.el
+++ lisp/x-hugh-python.el
@@ -25,26 +25,18 @@
 (-map (lambda (num) (* num num)) '(1 2 3 4))
 (funcall (-compose #'- #'1+ #'+) 1 2 3)
 
-
 (use-package lsp-pyright
   :ensure t
   :hook (python-mode . (lambda ()
 			 (require 'lsp-pyright)
 			 (lsp)))) 	;or lsp-deferred
 
-(defun x-hugh-highlight-indentation-mode-toggle ()
-  "Toggle whether highlight-indentation-mode is enabled.
-
-If highlight-indentation-mode is bound (as in, it is a function)
-and highlight-indentation-mode (the variable) is true, turn off;
-else, turn on."
-  (interactive)
-  (if (and (boundp 'highlight-indentation-mode)
-	   (eq highlight-indentation-mode t))
-      (highlight-indentation-mode -1)
-    (highlight-indentation-mode)))
-
-(x-hugh-highlight-indentation-mode-toggle)
+;; FIXME: Should move highlight-indent-guides to another file
+(use-package highlight-indent-guides
+  :ensure t
+  ;; :hook (prog-mode . (lambda ()
+  ;; 		       (highlight-indents-guide-mode)))
+  )
 
 (use-package python
   :custom ((python-indent-guess-indent-offset t)
