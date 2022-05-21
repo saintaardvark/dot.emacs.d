cdbcfb42117d73a4a573d344b3238abb402280bc
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2022-05-21 11:53:38 -0700

    Better use of use-package for projectile
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- lisp/x-hugh-projectile.el
+++ lisp/x-hugh-projectile.el
@@ -9,25 +9,27 @@
   ;; FIXME: Not sure what's going wrong.  Evaluating the progn works,
   ;; but not in with the rest of the use-package function.
   :ensure t
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
   :init
   (progn
     (mapc #'projectile-add-known-project
 	  (mapcar #'file-name-as-directory (magit-list-repos)))
     ;; Optionally write to persistent `projectile-known-projects-file'
     (projectile-save-known-projects))
-   ;; FIXME -- I like the helm-projectile, but I *really* want C-c p s for
+  ;; FIXME -- I like the helm-projectile, but I *really* want C-c p s for
   ;; a shell.
-)
-
-;; FIXME -- Can't remember why I have this here.  Do I need it?
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-
+  :custom
+  ;; http://endlessparentheses.com/improving-projectile-with-extra-commands.html
+  ;; FIXME: this might be the source of the other-window problem
+  (projectile-switch-project-action #'projectile-vc)
+  )
 
 ;; ;; TODO: Add shell as an option for projectile-commander (which is
 ;; ;; run after C-c p p)
 
-;; ;; TODO: Implement this helm-other-window fix:
-;; ;; https://emacs.stackexchange.com/questions/17072/open-file-in-a-specific-window
+;; TODO: Implement this helm-other-window fix:
+;; https://emacs.stackexchange.com/questions/17072/open-file-in-a-specific-window
 
 ;; Copy-pasta...there must be a better way to do this.
 
@@ -43,15 +45,9 @@
    ;; And now chef
    ((member project-type '(chef)) "_spec")))
 
-(setq projectile-test-suffix-function 'x-hugh-projectile-test-suffix)
-
 ;; TODO: Not working; suspect swallowed by projectile keymap.
 ;; TODO: Somehow get helm projectile while still have nice keyboard shortcut for shell
 ;; (global-set-key (kbd "C-c p p") 'helm-projectile-switch-project)
-
-;; http://endlessparentheses.com/improving-projectile-with-extra-commands.html
-(setq projectile-switch-project-action
-      #'projectile-vc)
 
 ;; Ermergerd:
 ;; `projectile-mode' Minor Mode Bindings Starting With C-c p x:
