f4b1e0665c22264662ea42d7d2a0e2d15500004b
Hugh Brown <hubrown@cisco.com>
2022-10-11 08:43:34 -0700

    Use spaces for indentation in dockerfile-mode

--- lisp/x-hugh-docker.el
+++ lisp/x-hugh-docker.el
@@ -5,6 +5,9 @@
 ;;; Code:
 
 (use-package dockerfile-mode
+  ;; Use spaces when indenting in dockerfile-mode
+  ;; TODO: This may be better as a directory variable.
+  :config (add-hook 'dockerfile-mode-hook (lambda () (setq indent-tabs-mode nil)))
   :ensure t)
 
 (provide 'x-hugh-docker)
