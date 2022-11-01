95057edb9b13e3e16843db1d8aaaf268da0c45f3
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2022-11-01 12:48:26 -0700

    Emergency fix for dired bug
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- lisp/x-hugh-dired.el
+++ lisp/x-hugh-dired.el
@@ -12,6 +12,38 @@
 	   (dired-recursive-copies 'always)
 	   (dired-recursive-deletes 'top)))
 
+;; Emergency fix for bug
+;; - https://debbugs.gnu.org/cgi/bugreport.cgi?bug=58946
+;; - https://debbugs.gnu.org/cgi/bugreport.cgi?bug=58870
+;; - https://snapcraft.io/emacs shows latest build being October 29th, 29.0.50-master-81d7827 -- this is what I have installed.
+;; Waiting on snapraft for this to be refreshed.
+
+(defun dired--make-directory-clickable ()
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward "^  /" nil t 1)
+      (let ((bound (line-end-position))
+            (segment-start (point))
+            (inhibit-read-only t)
+            (dir "/"))
+        (while (search-forward "/" bound t 1)
+          (setq dir (concat dir (buffer-substring segment-start (point))))
+          (add-text-properties
+           segment-start (1- (point))
+           `( mouse-face highlight
+              help-echo "mouse-1: goto this directory"
+              keymap ,(let* ((current-dir dir)
+                             (click (lambda ()
+                                      (interactive)
+                                      (if (assoc current-dir dired-subdir-alist)
+                                          (dired-goto-subdir current-dir)
+                                        (dired current-dir)))))
+                        (define-keymap
+                          "<mouse-2>" click
+                          "<follow-link>" 'mouse-face
+                          "RET" click)))))))))
+
+
 ;; TODO: Use dired-git-info
 (use-package dired-git-info
   :ensure t
