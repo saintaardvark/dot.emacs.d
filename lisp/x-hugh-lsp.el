a014073e76eb5405ab46f983a1d6ec9927e36fa0
Hugh Brown <hubrown@cisco.com>
2022-11-14 08:39:02 -0800

    Add comment about possible solution for lsp docstring nonsense

--- lisp/x-hugh-lsp.el
+++ lisp/x-hugh-lsp.el
@@ -4,6 +4,29 @@
 
 ;;; Code:
 
+;; TODO: Configure doc showing to not suck so much.  See
+;; https://www.reddit.com/r/emacs/comments/fxqfs2/trouble_with_lspmode_and_eldoc/
+;;
+;; Quote:
+;; When I leave my point within a function, its entire
+;; docstring will show in my minibuffer potentially using up 50+% of
+;; my screen. I've eventually figured that this is an eldoc feature,
+;; but I can't seem to disable it in any way. I found this issue on
+;; Github: https://github.com/emacs-lsp/lsp-mode/issues/443 , but it
+;; seems that setting lsp-eldoc-render-all has no effect for me even
+;; though it's nil. I also tried just disabling this feature by
+;; setting lsp-eldoc-enable-hover to nil, but again no luck. I still
+;; get the docstring in the minibuffer.
+;;
+;; One reply:
+;; I have this on my config, hope it works.
+;;
+;; (setq
+;;     lsp-signature-auto-activate t
+;;     lsp-signature-doc-lines 1)
+;;
+;; Another: check lsp-signature-auto-activate and lsp-signature-render-documentation
+
 (use-package lsp-mode
   :ensure t
   :commands (lsp lsp-deferred)
