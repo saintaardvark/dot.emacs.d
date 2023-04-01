8d6160317313424212c27681aeeeb58afe1c01e7
Hugh Brown (@hughbrownwyvern) <hugh.brown@wyvern.space>
2023-03-31 18:22:44 -0600

    NOMERGE: WIP: better x-hugh-jira function

--- lisp/x-hugh-jira.el
+++ lisp/x-hugh-jira.el
@@ -30,7 +30,52 @@ DNS-123[and now call this function]"
   (interactive)
   (save-excursion
     (backward-word 2)
-    (insert (format "%s/browse/"x-hugh-jira-url))))
+    (insert (format "%s/browse/" x-hugh-jira-url))))
+
+(defun x-hugh-make-link-out-of-ticket (beg end)
+  "Refactored version that's better."
+  (interactive "r")
+  (save-excursion
+    (let ((ticket (buffer-substring-no-properties beg end)))
+      (cond ((eq major-mode 'org-mode)
+	     (progn (delete-region beg end)
+		    (org-insert-link nil (format "%s/browse/%s" x-hugh-jira-url ticket) ticket)))
+	    ((eq major-mode 'markdown-mode)
+	     (progn (delete-region beg end)
+		    ;; I'm surprised there isn't a markdown-insert-link function...
+		    (insert (format "[%s][%s/browse/%s]" ticket x-hugh-jira-url ticket))))
+	    (t (message (format "I don't know how to make a link in %s!" major-mode)))))))
+
+(defun x-hugh-maybe-better-make-link-out-of-ticket (ticket)
+  "Refactored version that's better."
+  (interactive "sTicket DNS-")
+  (save-excursion
+    
+    (let* ((bounds (bounds-of-thing-at-point 'word))
+	   (beg (car bounds))
+	   (end (cdr bounds))
+	   (ticket (buffer-substring-no-properties (car bounds) (cdr bounds))))
+      (cond ((eq major-mode 'org-mode)
+	     (progn (delete-region beg end)
+		    (org-insert-link nil (format "%s/browse/%s" x-hugh-jira-url ticket) ticket)))
+	    ((eq major-mode 'markdown-mode)
+	     (progn (delete-region beg end)
+		    ;; I'm surprised there isn't a markdown-insert-link function...
+		    (insert (format "[%s][%s/browse/%s]" ticket x-hugh-jira-url ticket))))
+	    (t (message (format "I don't know how to make a link in %s!" major-mode)))))))
+
+            
+;; DNS-[[https://wyvernspace.atlassian.net/browse/123][123]]
+
+(defun x-hugh-make-markdown-link-out-of-ticket (beg end)
+  "Make org link out of ticket number."
+  (interactive "r")
+  (save-excursion
+    (let ((ticket (buffer-substring-no-properties beg end)))
+      (goto-char beg)
+      (insert "[")
+      (goto-char end)
+      (insert (format "](https://wyvernspace.atlassian.net/browse/%s)"ticket)))))
 
 (provide 'x-hugh-jira)
 
