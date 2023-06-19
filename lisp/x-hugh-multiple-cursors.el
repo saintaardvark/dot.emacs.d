0c5bf7aa0c3bedcdd313cf98b1b15acf50eacc04
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2023-06-19 07:12:55 -0700

    Add x-hugh-multiple-cursors, -treesitter.el
    x-hugh-combobulate added earlier.  Not calling these by default just yet.
    
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

---
+++ lisp/x-hugh-multiple-cursors.el
@@ -0,0 +1,12 @@
+;;; x-hugh-multiple-cursors
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package multiple-cursors
+  :ensure t)
+
+(provide 'x-hugh-multiple-cursors)
+
+;;; x-hugh-multiple-cursors.el ends here
