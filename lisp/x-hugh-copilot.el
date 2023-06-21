8a9856538e8ee2cda28f6ab52b343b172ba9b001
Hugh Brown (@hughbrownwyvern) <hugh.brown@wyvern.space>
2023-06-21 09:21:47 -0700

    Add x-hugh-copilot.el

---
+++ lisp/x-hugh-copilot.el
@@ -0,0 +1,13 @@
+;;; x-hugh-copilot --- Try github copilot
+
+;;; Commentary:
+;;; Is this the future?
+
+;;; Code:
+;; (use-package copilot
+;;   :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
+;;   :ensure t)
+(load-file "~/dev/src/github.com/zerolfx/copilot.el/copilot.el")
+
+(provide 'x-hugh-copilot)
+;;; x-hugh-copilot.el ends here
