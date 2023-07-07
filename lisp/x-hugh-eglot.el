4b7bc1bcc39034ea67a00ddd86c00d083f1059fb
Hugh Brown (@hughbrownwyvern) <hugh.brown@wyvern.space>
2023-07-07 11:09:44 -0700

    Try out eldoc-box, eldoc-overlay-mode

--- lisp/x-hugh-eglot.el
+++ lisp/x-hugh-eglot.el
@@ -9,5 +9,32 @@
   :defer t
   :hook (python-mode . eglot-ensure))
 
+;; Eldoc is used for documentation.  It's shown in the mode line by
+;; default, but there are a couple of packages that will do other
+;; things:
+;;
+;; https://repo.or.cz/eldoc-overlay.git
+
+;; Seems okay, but at first glance the font colour makes it pretty
+;; much unreadable.
+;;
+;; Toggle with: eldoc-overlay-mode
+;; (use-package eldoc-overlay
+;;   :ensure t
+;;   :delight eldoc-overlay-mode
+;;   :custom ((eldoc-overlay-backend 'inline-docs)
+;;            ;; (eldoc-overlay-delay 3)
+;;            )
+;;   :custom-face (inline-docs-border-face ((t (:family "DejaVu Sans Mono"))))
+;;   :hook (eldoc-mode . eldoc-overlay-mode))
+
+;; eldoc-box
+;;
+;; toggle with: eldoc-box-hover-mode
+;; (but also eldoc-box-hover-at-point-mode, eldoc-box-help-at-point)
+(use-package eldoc-box
+  :ensure t)
+(add-hook 'eglot-managed-mode-hook #'eldoc-box-hover-mode t)
+
 (provide 'x-hugh-eglot)
 ;;; x-hugh-eglot.el ends here
